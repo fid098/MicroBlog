e3af79bea99ec57186d9aa7be0a6db8d773fa842
Fidel <fidel.ehirim@student.manchester.ac.uk>
2025-12-04 00:14:11 +0000

    base template created and jinja style for loop

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,13 @@ def index():
 #when a user accesses the root URL or /index, the index() function is called and "Hello, world" is returned as the response
 #the app.route() decorator takes the URL pattern as an argument and associates it with the index() function
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {'author': {'username': 'John'}, 
+         'body': 'Beautiful day in Portland!'},
+        {'author': {'username': 'Susan'}, 
+         'body': 'The Avengers movie was so cool!'}
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 #this converts a template file (index.html) into a complete HTML page
 #the render_template() function takes the name of the template file as its first argument
 #additional arguments are key-value pairs that are passed to the template engine
