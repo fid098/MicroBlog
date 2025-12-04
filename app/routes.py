7db5d57d61c115339edb2a0c170de3669c5ed658
Fidel <fidel.ehirim@student.manchester.ac.uk>
2025-12-04 00:04:07 +0000

    first template and control statement

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,18 @@
 from app import app
+from flask import render_template
 
 #this is a view function(handlers for the application routes)
 @app.route('/')  #this is a decorator that modifies the function that follows it 
 @app.route('/index')  #this binds the URL /index to the index() function
 def index():
-    return "Hello, world"
 #the route() decorator in flask is used to bind a function to a URL
 #when a user accesses the root URL or /index, the index() function is called and "Hello, world" is returned as the response
 #the app.route() decorator takes the URL pattern as an argument and associates it with the index() function
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
+#this converts a template file (index.html) into a complete HTML page
+#the render_template() function takes the name of the template file as its first argument
+#additional arguments are key-value pairs that are passed to the template engine
+#these key-value pairs can be used within the template to dynamically generate content
+
 
