0be683ca057028bad50d4c077e60c90ffca3438d
Fidel <fidel.ehirim@student.manchester.ac.uk>
2025-12-05 02:34:19 +0000

    Set up Login and Registeration system using flask-login, wtfforms and werkzeug security

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,24 @@
-from app import app
-from flask import render_template
-from app.forms import LoginForm
+from app import app, db
+from flask import render_template, request
+from app.forms import LoginForm, RegistrationForm
 #i import the LoginForm class from the forms module in the app package
+#i import the RegistrationForm class from the forms module in the app package
 from flask import flash, redirect, url_for
 #these are imported to handle flashing messages and redirecting users to different routes
+from flask_login import current_user, login_user, logout_user, login_required
+#these are imported to manage user login sessions
+from app.models import User
+import sqlalchemy as sa
+from urllib.parse import urlsplit
+#this module defines the routes (URL endpoints) for the Flask web application
+
 
 
 #this is a view function(handlers for the application routes)
 @app.route('/')  #this is a decorator that modifies the function that follows it 
 @app.route('/index')  #this binds the URL /index to the index() function
+@login_required
+#this decorator ensures that only authenticated (logged-in) users can access this route
 def index():
 #the route() decorator in flask is used to bind a function to a URL
 #when a user accesses the root URL or /index, the index() function is called and "Hello, world" is returned as the response
@@ -20,7 +30,7 @@ def index():
         {'author': {'username': 'Susan'}, 
          'body': 'The Avengers movie was so cool!'}
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 #this converts a template file (index.html) into a complete HTML page
 #the render_template() function takes the name of the template file as its first argument
 #additional arguments are key-value pairs that are passed to the template engine
@@ -29,12 +39,53 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 #this binds the URL /login to the login() function and allows both GET and POST methods
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    #if the user is already logged in (authenticated), redirect them to the index page
     form = LoginForm()
     #this creates an instance of the LoginForm class defined in app/forms.py
     if form.validate_on_submit():
         #this checks if the form has been submitted and if the data is valid according to the validators defined in the form class
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        #this queries the database for a user with the username provided in the form
+        if user is None or not user.check_password(form.password.data):
+            #if no such user exists or the password is incorrect, flash an error message and redirect back to the login page
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        #this logs in the user and sets a session cookie
+        #the remember parameter indicates whether to remember the user across sessions
+        next_page = request.args.get('next')
+        #this gets the next page the user was trying to access before being redirected to the login page
+        if not next_page or urlsplit(next_page).netloc != '':
+        #urlsplit is used to parse the URL and check its components
+        #netloc checks if the URL has a network location (domain) and if it is safe to redirect to
+            next_page = url_for('index')
+        return redirect(next_page)
+        #redirect the user to the next page or the index page if no next page is specified
     return render_template('login.html', title='Sign In', form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    #this logs out the current user by removing their session cookie
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    #if the user is already logged in (authenticated), redirect them to the index page
+    form = RegistrationForm()
+    #this creates an instance of the RegistrationForm class defined in app/forms.py
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        #this creates a new User object with the data from the form
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+        #flash a success message and redirect the user to the login page
+    return render_template('register.html', title='Register', form=form)
